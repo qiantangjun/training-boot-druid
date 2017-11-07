92d71e775fb8d0db783071bf10f86fdfd01bf0bb
Percy0601 <2008dreams@2008.sina.com>
2017-11-07 09:56:49 +0800

    更新导入

--- src/main/java/io/training/boot/druid/SqlReadWriteFilter.java
+++ src/main/java/io/training/boot/druid/SqlReadWriteFilter.java
@@ -1,29 +1,14 @@
 package io.training.boot.druid;
 
 import com.alibaba.druid.filter.FilterEventAdapter;
-import com.alibaba.druid.proxy.jdbc.StatementExecuteType;
 import com.alibaba.druid.proxy.jdbc.StatementProxy;
 import com.alibaba.druid.sql.SQLUtils;
-import com.alibaba.druid.sql.ast.*;
-import com.alibaba.druid.sql.ast.expr.*;
-import com.alibaba.druid.sql.ast.statement.*;
-import com.alibaba.druid.sql.dialect.mysql.ast.*;
-import com.alibaba.druid.sql.dialect.mysql.ast.clause.*;
-import com.alibaba.druid.sql.dialect.mysql.ast.expr.*;
-import com.alibaba.druid.sql.dialect.mysql.ast.statement.*;
-import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
-import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitor;
-import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
-import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
-import com.alibaba.druid.stat.TableStat;
-import com.alibaba.druid.support.json.JSONParser;
 import com.alibaba.druid.util.JdbcConstants;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 public class SqlReadWriteFilter extends FilterEventAdapter {
